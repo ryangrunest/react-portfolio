5e2e5c4631015e0320f37db1b337f2582b6fea0b
Ryan Grunest <ryangrunest45@gmail.com>
2020-02-17 15:42:02 -0800

    blog styling

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f62b418efa9db2e2c45a";
+/******/ 	var hotCurrentHash = "e8258ebf622347801c5f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
