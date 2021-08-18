97941c5fc9cabe244a5fbea9d256cab299d6abe3
Ryan Grunest <ryangrunest@geeko>
2021-08-18 16:10:14 -0600

    fix css defect on index page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "14c747a6095e7e108558"; }
+/******/ 		__webpack_require__.h = function() { return "10dce66c4669a634fa3c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,17 @@ import Art from "./components/Art";
 
 export default function Index() {
   return (
-    <Layout page="Home" wrapper="index">
-      <div className="github-profile"></div>
-      <div className="overlay">
-        <h1>Software / Web Developer</h1>
-        <p>Currently Based in Portland, OR.</p>
-        <p>
-          Don't be shy. Send me an{" "}
-          <a href="mailto:ryangrunest45@gmail.com">email</a>.
-        </p>
+    <Layout page="Home" wrapper="home">
+      <div className="index">
+        <div className="github-profile"></div>
+        <div className="overlay">
+          <h1>Software / Web Developer</h1>
+          <p>Currently Based in Portland, OR.</p>
+          <p>
+            Don't be shy. Send me an{" "}
+            <a href="mailto:ryangrunest45@gmail.com">email</a>.
+          </p>
+        </div>
       </div>
       <Who />
       <Art />
