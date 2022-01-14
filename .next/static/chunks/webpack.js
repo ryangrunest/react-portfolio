76745fdbb132f79fad8c67ee2cc7623525bb5410
Ryan Grunest <ryangrunest@geeko>
2022-01-14 10:53:17 -0800

    fix immer dep warning

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3cf575712fbe112e"; }
+/******/ 		__webpack_require__.h = function() { return "cc6cdb2cab983199"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
